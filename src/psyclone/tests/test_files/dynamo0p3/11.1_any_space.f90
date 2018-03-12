e034b00bfde25424546b027c814000b121d16745
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-11 19:09:30 -0500

    delete
    Conflicts:
    	changelog
    	psyclone.pdf

--- src/psyclone/tests/test_files/dynamo0p3/11.1_any_space.f90
+++
@@ -1,8 +0,0 @@
-program any_space_example
-
-  use testkern_any_space_2_mod, only : testkern_any_space_2_type
-  integer :: istp
-
-  call invoke(testkern_any_space_2_type(a, b, c, istp))
-
-end program any_space_example
